09cf849bf1e00cf0e8705bdf18cb3d05eb92e61d
Tamara Torrecillas <tamaratgz@gmail.com>
2022-08-28 21:29:19 +0200

    Bug fix for Sí and Si

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,15 +5,17 @@ class QuestionsController < ApplicationController
   def answer
     @youranswer = params[:youranswer].capitalize
     if @youranswer.blank?
-      @myanswer = "😶 No has respondido... ¡Prueba otra vez!"
-    elsif @youranswer == "Sí" || "Si"
-      @myanswer = "🎉 ¡Qué ilusión! Tienes mis datos de contacto abajo. Espero tus noticias."
-    elsif @youranswer == "No"
-      @myanswer = "😕 Oh, me da mucha pena, ¡pero seguiré aprendiendo y lo seguiré intentando!"
+      @myanswer = '😶 No has respondido... ¡Prueba otra vez!'
+    elsif @youranswer == 'Sí'
+      @myanswer = '🎉 ¡Qué ilusión! Tienes mis datos de contacto abajo. Espero tus noticias.'
+    elsif @youranswer == 'Si'
+      @myanswer = '🎉 ¡Qué ilusión! Tienes mis datos de contacto abajo. Espero tus noticias.'
+    elsif @youranswer == 'No'
+      @myanswer = '😕 Oh, me da mucha pena, ¡pero seguiré aprendiendo y lo seguiré intentando!'
     elsif @youranswer.ends_with?("?")
-      @myanswer = "🤔 Esto de responder a una pregunta con otra es muy gallego, ¿no?"
+      @myanswer = '🤔 Esto de responder a una pregunta con otra es muy gallego, ¿no?'
     else
-      @myanswer = "Te invito a que vuelvas a responder con 'Sí'. Pero no dudes en ver qué pasa si dejas tu respuesta en blanco o si dices 'No'. 🙌"
+      @myanswer = 'Te invito a que vuelvas a responder con "Sí". Pero no dudes en ver qué pasa si dejas tu respuesta en blanco o si dices "No". 🙌'
     end
   end
 end
