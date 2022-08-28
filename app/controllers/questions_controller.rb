183217b2980b896ee3634b0bd081311762eef0ef
Tamara Torrecillas <tamaratgz@gmail.com>
2022-08-28 21:13:39 +0200

    Capitalize answer and ignore accent

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,17 +3,17 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @youranswer = params[:youranswer]
+    @youranswer = params[:youranswer].capitalize
     if @youranswer.blank?
       @myanswer = "😶 No has respondido... ¡Prueba otra vez!"
-    elsif @youranswer == "Sí"
+    elsif @youranswer == "Sí" || "Si"
       @myanswer = "🎉 ¡Qué ilusión! Tienes mis datos de contacto abajo. Espero tus noticias."
     elsif @youranswer == "No"
       @myanswer = "😕 Oh, me da mucha pena, ¡pero seguiré aprendiendo y lo seguiré intentando!"
     elsif @youranswer.ends_with?("?")
       @myanswer = "🤔 Esto de responder a una pregunta con otra es muy gallego, ¿no?"
     else
-      @myanswer = "Te invito a que vuelvas a responder con 'Sí' (con la tilde). Pero no dudes en ver qué pasa si dejas tu respuesta en blanco o si dices 'No'. 🙌"
+      @myanswer = "Te invito a que vuelvas a responder con 'Sí'. Pero no dudes en ver qué pasa si dejas tu respuesta en blanco o si dices 'No'. 🙌"
     end
   end
 end
