3d3481a1bd79f1c59303f00d93993aecbed07af2
Tamara Torrecillas <tamaratgz@gmail.com>
2022-08-28 18:42:11 +0200

    design

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,15 +5,15 @@ class QuestionsController < ApplicationController
   def answer
     @youranswer = params[:youranswer]
     if @youranswer.blank?
-      @tamaraanswer = "No has respondido... ¡Prueba otra vez!"
+      @myanswer = "😶 No has respondido... ¡Prueba otra vez!"
     elsif @youranswer == "Sí"
-      @tamaraanswer = "¡Qué ilusión! Tienes mis datos de contacto abajo. Espero tus noticias. ☺️"
+      @myanswer = "🎉 ¡Qué ilusión! Tienes mis datos de contacto abajo. Espero tus noticias."
     elsif @youranswer == "No"
-      @tamaraanswer = "Oh, me da mucha pena, ¡pero seguiré aprendiendo y lo seguiré intentando!"
+      @myanswer = "😕 Oh, me da mucha pena, ¡pero seguiré aprendiendo y lo seguiré intentando!"
     elsif @youranswer.ends_with?("?")
-      @tamaraanswer = "Esto de responder a una pregunta con otra es muy gallego, ¿no?"
+      @myanswer = "🤔 Esto de responder a una pregunta con otra es muy gallego, ¿no?"
     else
-      @tamaraanswer = "Te invito a que vuelvas a responder con 'Sí' (con la tilde). Pero no dudes en ver qué pasa si dejas tu respuesta en blanco y dices 'No'. 🙌"
+      @myanswer = "Te invito a que vuelvas a responder con 'Sí' (con la tilde). Pero no dudes en ver qué pasa si dejas tu respuesta en blanco o si dices 'No'. 🙌"
     end
   end
 end
